4861cdf675e37442d769f09445385870d236f8c4
João Rodrigues <johnnyrod4569@gmail.com>
2025-05-21 16:19:16 +0200

    feat: Add All Django allauth templates to the projects templates folder for customisation.

--- farm_fresh_v_2_0_project/farm_fresh_v_2_0_project/settings.py
+++ farm_fresh_v_2_0_project/farm_fresh_v_2_0_project/settings.py
@@ -79,6 +79,8 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                # For customising the `allauth` templates
+                "django.template.context_processors.debug",
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
