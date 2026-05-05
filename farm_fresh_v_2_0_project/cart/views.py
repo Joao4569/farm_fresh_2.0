f47d81602197fe558d21e06732a1aa6393f04907
João Rodrigues <johnnyrod4569@gmail.com>
2026-05-05 13:25:16 +0200

    feat: Remove unneeded success toast from cart view for a better user experience, no need to duplicate info to the user.

--- farm_fresh_v_2_0_project/cart/views.py
+++ farm_fresh_v_2_0_project/cart/views.py
@@ -38,18 +38,13 @@ def adjust_cart(request, item_id):
     """Adjust the quantity of the specified product to the specified
     user input"""
 
-    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
-        messages.success(request, f'Updated {product.name} \
-            quantity to {cart[item_id]}')
     else:
         cart.pop(item_id)
-        messages.success(request, f'Removed {product.name} \
-            from your shopping cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -58,13 +53,10 @@ def adjust_cart(request, item_id):
 def remove_from_cart(request, item_id):
     """Remove the item from the shopping cart"""
 
-    product = get_object_or_404(Product, pk=item_id)
     try:
         cart = request.session.get('cart', {})
 
         cart.pop(item_id)
-        messages.success(request, f'Removed {product.name} \
-            from your shopping cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
